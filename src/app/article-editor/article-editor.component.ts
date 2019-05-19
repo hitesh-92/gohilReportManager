0c88f99a1d57ef2ed6207b79b40594a8ab206e18
hitesh <hiteshgohil@hotmail.co.uk>
2019-05-19 18:48:07 +0100

    UI update for achived article

--- src/app/article-editor/article-editor.component.ts
+++ src/app/article-editor/article-editor.component.ts
@@ -22,7 +22,50 @@ export class ArticleEditorComponent implements OnInit {
     { value: 'Alert', viewValue: 'Alert' }
   ];
 
+  articleTitle = 'Color-hex.com also generates a simple css code for the selected color. Html element samples are also shown below the color detail page.';
+  articleUrl = 'http://www.example.com';
+  imageUrl = 'http://somwhere.com/someimageLink';
+  articleColumn = 'left';
+  isArchived = true;
+
+  allowUpdate = true; //allow if validated
+
+  //update article values
+  updatedArticleTitle = '';
+  updatedArticleURL = '';
+  updatedImageURL = '';
+
+  // edit editor body color if archived
+  colorEditor(){
+    return this.isArchived == false ? 'white' : '#fbf6d9';
+  }
+
   ngOnInit() {
+  }
+
+  //  article update data
+  onUpdateArticleTitle({target: {value}}){
+
+    // console.log(value)
+    this.updatedArticleTitle = value;
+  }
+
+  //  buttons
+
+  onUpdate(){
+    console.log('DO you really want to UPDATE this?');
+  }
+
+  onArchive(){
+    console.log('DO you really want to ARCHIVE this?');
+  }
+
+  onDelete(){
+    console.log('DO you really want to DELETE this?');
+  }
+
+  onUnarchive(){
+    console.log('DO you really want to UN-ARCHIVE this?')
   }
 
 }
