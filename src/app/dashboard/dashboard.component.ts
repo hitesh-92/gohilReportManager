05a50b3804d7f1d0e0daa9ad49c0a4be86779708
hitesh <hiteshgohil@hotmail.co.uk>
2019-05-24 16:28:55 +0100

    added alert counter to overview

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,29 +10,29 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const token: any = checkForToken();
+    // const token: any = checkForToken();
     // console.log(`dashboard token: ${token}`);
     // if no token redirect to '/'
 
-    if(token){
-      getAllData(token).then(res => console.log(res) )
-    }
+    // if(token){
+    //   getAllData(token).then(res => console.log(res) )
+    // }
 
     // -----
 
-    function checkForToken(){
-      return window.sessionStorage.getItem('token');
-    };
+    // function checkForToken(){
+    //   return window.sessionStorage.getItem('token');
+    // };
 
-    async function getAllData(token){
-      const url: string = 'http://localhost:8000/column/';
-      const ops: object = {
-        method: 'GET',
-        mode: 'cors',
-        headers: {'x-auth': token}
-      }
-      return await fetch(url, ops).then(r => r.json());
-    };
+    // async function getAllData(token){
+    //   const url: string = 'http://localhost:8000/column/';
+    //   const ops: object = {
+    //     method: 'GET',
+    //     mode: 'cors',
+    //     headers: {'x-auth': token}
+    //   }
+    //   return await fetch(url, ops).then(r => r.json());
+    // };
 
   };
 
