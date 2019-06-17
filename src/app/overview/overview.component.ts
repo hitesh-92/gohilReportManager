a2a0474d7bfc25008b7f972ff4743150ab477f5b
hitesh <hiteshgohil@hotmail.co.uk>
2019-06-17 10:55:22 +0100

    article counts displayed with databinding

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -8,7 +8,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class OverviewComponent implements OnInit {
 
-  articles: object = [
+  articles: any = [
     {title: "Beijing pioneering citizens' 'points' system critics brand 'Orwellian'"},
     {title: "Almost Half of U.S. Births Happen Outside Marriage, Signaling Cultural Shift"}
   ]
@@ -24,35 +24,66 @@ export class OverviewComponent implements OnInit {
     alert: {}
   };
 
-  /*
-  *
-  *   Display with databinding
-  *
-  */
+  counts: any = {
+    alert: null,
+    left: null,
+    center: null,
+    right: null,
+    archive: null,
+    total: 0
+  }
 
+  status: any = {
+    new: 0,
+    oneMonth: 0,
+    threeMonth: 0,
+    sixMonth: 0
+  }
+  
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.fetchColumn('left');
-    this.fetchColumn('center');
-    this.fetchColumn('right');
-    this.fetchColumn('alert');
-    this.fetchColumn('archive');
+    this.fetchAllColumns();
     console.log(this.data)
   }
 
   ngOnChanges(){
+    this.fetchAllColumns();
+  }
+
+  fetchAllColumns(){
     this.fetchColumn('left');
     this.fetchColumn('center');
     this.fetchColumn('right');
     this.fetchColumn('alert');
     this.fetchColumn('archive');
+  }
+
+  calculateTotal(){
+    let total:number = 0;
+    for(let i in this.counts){
+      if(this.counts[i] !== null) total += this.counts[i]
+    }
+    return total;
   }
 
   private fetchColumn(title: string){
     return this.http
     .get(`http://localhost:8000/column/${title}`)
-    .subscribe(resp => this.data[title] = resp );
+    .subscribe((resp:any) => {
+      this.data[title] = resp
+      this.counts[title] = resp.articles.length;
+      this.counts.total += resp.articles.length;
+
+      resp.articles.forEach(({status}) => this.handleStatusCheck(status))
+    });
+  }
+
+  handleStatusCheck(status:number){
+    if(status === 0) this.status.new += 1;
+    else if(status === 1) this.status.oneMonth += 1;
+    else if(status === 2) this.status.threeMonth += 1;
+    else if(status === 3) this.status.sixMonth += 1;
   }
 
   clearInputs(){
