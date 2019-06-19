9ee8ddd219ffaf5ccc73e520c7d2c16fc3a583ed
hitesh <hiteshgohil@hotmail.co.uk>
2019-06-19 10:27:31 +0100

    route added: right column

---
+++ src/app/right-column/right-column.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, OnChanges } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-right-column',
+  templateUrl: './right-column.component.html',
+  styleUrls: ['./right-column.component.css']
+})
+export class RightColumnComponent implements OnInit {
+
+  columnTitle: string = ''
+  columnId: string = ''
+  articles: any = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fetchColumn()
+  }
+
+  ngOnChanges(){
+    this.fetchColumn()
+  }
+
+  fetchColumn(){
+    this.http
+    .get(
+      'http://localhost:8000/column/right',
+      {
+        headers: new HttpHeaders({
+          'auth': window.sessionStorage.getItem('token')
+        })
+      }
+    )
+    .subscribe( (resp: any) => {
+      // console.log('/right succes ==> ', resp)
+
+      if (resp.error) return console.error('left-column, ERROR, fetchColumn()');
+
+      this.articles = [...resp.articles];
+      this.columnTitle = resp.columnData.title;
+      this.columnId = resp.columnData._id;
+    });
+
+  }
+
+}
