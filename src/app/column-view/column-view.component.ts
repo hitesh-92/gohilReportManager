067915bf5d0bb9e2f8b0798987748705dfae002b
hitesh <hiteshgohil@hotmail.co.uk>
2019-06-25 20:07:10 +0100

    reesigned article display using table

--- src/app/column-view/column-view.component.ts
+++ src/app/column-view/column-view.component.ts
@@ -3,10 +3,10 @@ import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 
-import {DomSanitizer} from '@angular/platform-browser';
-import {MatIconRegistry} from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-column-view',
@@ -26,8 +26,10 @@ export class ColumnViewComponent implements OnInit {
   allowSwitch: boolean = false;
 
   //stores index of article in articles array
-  switch: number = -1;
+  selected: number = -1;
   moveTo: number = -1;
+  edit: boolean = true;
+  requestType: any = '';
 
   //table
   displayedColumns: string[] = ['position', 'title', 'url', 'image', '_id'];
@@ -36,11 +38,7 @@ export class ColumnViewComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  constructor(private http: HttpClient, private location: Location, private router: Router, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon(
-        'thumbs-up',
-        sanitizer.bypassSecurityTrustResourceUrl('assets/img/examples/thumbup-icon.svg'));
-  }
+  constructor(private http: HttpClient, private location: Location, private router: Router) { }
 
   ngOnInit() {
 
@@ -55,7 +53,37 @@ export class ColumnViewComponent implements OnInit {
     this.dataSource = new MatTableDataSource(this.articles);
   }
 
+
+  /*
+  *   Buttons
+  */
+
   onEdit(position: any){
+    if(this.edit === true) this.forwardToEditPage(position);
+    else this.handleSwitchSelects(position, true);
+  }
+
+  onSwitch(position: number){
+    this.handleSwitchSelects(position, false);
+  }
+
+  onConfirmSwitch(event:any){
+    // console.log(`TYPE:${this.requestType}, selected:${this.selected}, moveTo:${this.moveTo}`);
+
+    const body: any = {
+      selected: this.articles[this.selected - 1]._id,
+      moveTo: this.articles[this.moveTo - 1]._id
+    };
+
+    if( this.requestType === 'switch' ) this.handleSwitchRequest(body);
+    else if ( this.requestType === 'insert' ) this.handleInsertRequest(body);
+  }
+
+  /*
+  *   Navigation
+  */
+
+  forwardToEditPage(position: number){
     const current: string = location.pathname;
     const articleId: string = this.articles[position - 1]._id;
     const navigateTo: string = `${current}/editor/${articleId}`;
@@ -68,80 +96,135 @@ export class ColumnViewComponent implements OnInit {
     this.router.navigate([navigateTo]);
   }
 
-  onSwitch(event: any){
+  /*
+  *   HTTP
+  */
 
-    console.log(event)
-
-    // let article = this.findArticleFromEvent(event);
-    // let index = article.position - 1;
-    //
-    // console.log('index: ',index, ' / ',  article['title']);
-    //
-    // // handle select
-    // if(this.switch === -1) this.switch = index;
-    // else if(this.switch === index) this.switch = -1;
-    // else {
-    //   //handle moveTo
-    //   if(this.moveTo === -1) this.moveTo = index;
-    //   else if(this.moveTo === index) this.moveTo = -1
-    // }
-    //
-    // if(this.switch !== -1 && this.moveTo !== -1) this.allowSwitch = true;
-    // else this.allowSwitch = false;
-  }
-
-  findArticleFromEvent(event:any){
-
-    let position: any = event.target.id === ""
-    ? event.target.parentElement.parentElement.parentElement.children["0"].innerText
-    : event.target.id
-
-    let index = parseInt(position) - 1;
-
-    return this.articles[index];
-  }
-
-  switchPositionDisplay(title:string){
-
-    if(title === 'selected'){
-      return this.switch === -1
-      ? '0'
-      : `${this.switch + 1}`
-    } else {
-      return this.moveTo === -1
-      ? '0'
-      : `${this.moveTo + 1}`
-    }
-
-  }
-
-  onConfirmSwitch(event:any){
-
-    if(this.switch === -1 && this.moveTo === -1) return;
-
-    let body: any = {
-      selected: this.articles[this.switch]._id,
-      moveTo: this.articles[this.moveTo]._id
-    };
-
-    this.submitSwitchRequest(body);
-  }
-
-  private submitSwitchRequest(body: any){
+  private handleSwitchRequest(body: any){
     const url: string = 'http://localhost:8000/article/switch';
-    let headers = new HttpHeaders({
+    const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'x-auth': window.sessionStorage.getItem('token')
     });
 
-    this.http
-    .patch(url, body, { headers })
+    this.http.patch(url, body, { headers })
     .subscribe( (resp:any) => {
-      console.log('Switch Success ==>', resp)
-      let loc: string = `/app/${this.columnTitle}-column`;
-      if(resp.status) location.reload();
+      console.log('Switch Success ==> ', resp);
+      this.resetColumnArticles();
+    });
+  }
 
-    })
+  private handleInsertRequest(body: any){
+    const url: string = 'http://localhost:8000/article/insert';
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'x-auth': window.sessionStorage.getItem('token')
+    });
+
+    this.http.patch(url, body, { headers })
+    .subscribe( (resp:any) => {
+      console.log('Insert Success ==> ', resp);
+      this.resetColumnArticles();
+    });
+  }
+
+  private resetColumnArticles(){
+    const url: string = `http://localhost:8000/column/${this.columnTitle}`;
+    const headers = new HttpHeaders({ 'x-auth': window.sessionStorage.getItem('token') });
+
+    this.http.get(url, { headers })
+    .subscribe( (resp: any) => {
+      this.articles = [...resp.articles];
+      // ADD IN LOADING
+      this.selected = -1;
+      this.moveTo = -1;
+      this.dataSource = new MatTableDataSource(this.articles);
+    });
+  }
+
+  /*
+  *   Logic
+  */
+
+  switchPositionDisplay(title:string){
+
+    if(title === 'selected'){
+      return this.selected === -1
+      ? '0'
+      : `${this.selected}`
+    } else {
+      return this.moveTo === -1
+      ? '0'
+      : `${this.moveTo}`
+    }
+
+  }
+
+  updateSwitchIcons(){
+    this.selected === -1 && this.moveTo === -1
+    ? this.edit = true
+    : this.edit = false
+  }
+
+  handleSwitchSelects(position: number, toInsert: boolean){
+
+    if( toInsert === false ){
+      // handleInitialSelect then handleSwitchArticles
+
+      const selectedInitialAgain: boolean = this.selected === position;
+      const newSelect: boolean = this.selected === -1;
+
+      if( selectedInitialAgain ) this.selected = -1;
+      else if( newSelect ) this.selected = position;
+      else{
+        //handleSwitchArticles
+
+        // const sameAsSelected: boolean = this.selected === position;
+        const selectedMoveToAgain: boolean = this.moveTo === position;
+        const newMoveTo: boolean = this.moveTo === -1;
+
+        // if ( sameAsSelected ) {return console.log('picked again switch')}
+        if( selectedMoveToAgain ) {
+          this.moveTo = -1;
+          this.requestType = '';
+        }
+        else if ( newMoveTo ) {
+          this.moveTo = position;
+          this.requestType = 'switch';
+        }
+        else console.log('ERROR - handleSwitchSelects -> handleSwitchArticles');
+
+      }
+
+    } else {
+      //handleInsertArticle
+
+      const sameAsSelected: boolean = this.selected === position;
+      const selectedInsertAgain =  this.moveTo === position;
+      const newInsert = this.moveTo === -1;
+
+      if ( sameAsSelected ) {return console.log('picked again insert')}
+      else if( selectedInsertAgain ) {
+        this.moveTo = -1;
+        this.requestType = '';
+      }
+      else if( newInsert ) {
+        this.moveTo = position;
+        this.requestType = 'insert';
+      }
+      else console.log('ERROR - handleSwitchSelects -> handleInsertArticle');
+
+    }
+
+    console.log(`TYPE:${this.requestType}, selected:${this.selected}, moveTo:${this.moveTo}`);
+
+    this.handleAllowConfirm();
+    this.updateSwitchIcons();
+  }
+
+  handleAllowConfirm(){
+    if(this.selected !== -1 && this.moveTo !== -1) this.allowSwitch = true;
+    else this.allowSwitch = false;
   }
 
 }
