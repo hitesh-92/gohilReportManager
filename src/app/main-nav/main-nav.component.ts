7c8139a45b05a809ddfd4ba643420e3348c34996
hitesh <hiteshgohil@hotmail.co.uk>
2019-07-04 00:52:48 +0100

    tmp loguot ubtton

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -17,6 +21,18 @@ export class MainNavComponent {
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private router: Router, private http: HttpClient) {}
+
+  onHandleLogout(){
+
+    const url: string = 'http://localhost:8000/user/logout';
+    const token: string = sessionStorage.getItem('token');
+    const body: any = { token: token };
+    const headers: any = new HttpHeaders({ 'x-auth': token });
+
+    console.log('Add Logout functionality. main-nav.ts ==> ', body);
+
+    // this.http.post(url, body, { headers })
+  }
 
 }
