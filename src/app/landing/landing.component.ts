685e97374bf63bc22fe650bda0ec8d5c9c2db039
hitesh <hiteshgohil@hotmail.co.uk>
2019-07-06 14:00:14 +0100

    valid token navigates user to overview

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    const token: any = sessionStorage.getItem('token');
+    if ( token === null ) return;
+
+    this.attemptSignIn(token);
+  }
+
+  private attemptSignIn(token: string){
+
+    const url: string = 'http://localhost:8000/user/login';
+    const body: {} = {};
+    const headers = new HttpHeaders({'x-auth': token});
+
+    this.http.post(url, body, { headers })
+    .subscribe( (resp: any) => {
+
+      if( resp.loggedIn === true ){
+        this.setToken(resp.token);
+        this.router.navigate(['/app/overview']);
+      }
+    });
+
+  }
+
+  setToken(token: string){
+    window.sessionStorage.setItem('token', token);
   }
 
 }
